228fd76d9711b429d38a91cf2e216abd0f7ecf55
ruijpedro <rjpedro@gmail.com>
2026-07-03 01:23:19 +0100

    Update google.js

--- src/services/google.js
+++ src/services/google.js
@@ -4,10 +4,7 @@ const SCOPES = [
   'profile',
   'https://www.googleapis.com/auth/calendar.events',
   'https://www.googleapis.com/auth/tasks',
-  'https://www.googleapis.com/auth/contacts.readonly',
-  'https://www.googleapis.com/auth/gmail.send',
-  'https://www.googleapis.com/auth/drive.file',
-  'https://www.googleapis.com/auth/spreadsheets'
+  'https://www.googleapis.com/auth/contacts.readonly'
 ]
 
 let tokenClient = null
